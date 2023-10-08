ed57608b4d475c71bf68375f5bbb586abc919f7b
Luisa Chiu <luisachiu1@gmail.com>
2023-10-07 19:30:40 -0700

    Finished HW1

--- HW1/process.py
+++ HW1/process.py
@@ -15,70 +15,8 @@ path_out = os.path.basename(args.path).split('.')[0]+'.JPG'
 with rawpy.imread(args.path) as raw:
     # raw_image contains the raw image data in 16-bit integer format.
     raw_image = raw.raw_image
-    normalized_img = raw_image.astype(np.float32) / (2**16 - 1)
-    original = np.array([[100, 20, 30, 40], [80, 10, 70, 20], [10, 20, 30, 20], [200, 70, 80, 90]])
-    padded = np.pad(original, pad_width=1, mode='constant', constant_values=0)
-    print("Padded Array:")
-    print(padded)
-    row = padded.shape[0] # num of rows
-    col = padded.shape[1] # num of col
-    Red = []
-    Blue = []
-    Green = []
-    Red_array= np.empty((original.shape[0], original.shape[1]))
-    Blue_array= np.empty((original.shape[0], original.shape[1]))
-    Green_array= np.empty((original.shape[0], original.shape[1]))
-    for i in range(1, row-1, 1): 
-        for j in range(1, col-1, 1):
-            # At red pixel
-            if i%2 != 0 and j%2 != 0:
-                Red.append(padded[i, j])
-                Red_array[i-1 ,j-1] = padded[i, j]
-                Green.append((1/4)*(padded[i+1, j] + padded[i-1, j] + padded[i, j-1] + padded[i, j+1]))
-                Green_array[i-1 ,j-1] = (1/4)*(padded[i+1, j] + padded[i-1, j] + padded[i, j-1] + padded[i, j+1])
-                Blue.append((1/4)*(padded[i+1, j+1] + padded[i-1, j-1]+ padded[i-1, j+1] + padded[i+1, j-1]))
-                Blue_array[i-1 ,j-1] = (1/4)*(padded[i+1, j+1] + padded[i-1, j-1]+ padded[i-1, j+1] + padded[i+1, j-1])
-            # At green pixel with red adjacent
-            elif i%2 != 0 and j%2 == 0:
-                Red.append((1/2)*(padded[i, j-1] + padded[i, j+1]))
-                Red_array[i-1,j-1] = (1/2)*(padded[i, j-1] + padded[i, j+1])
-                Green.append(padded[i,j])
-                Green_array[i-1 ,j-1] = padded[i,j]
-                Blue.append((1/2)*(padded[i-1, j]+padded[i+1, j]))
-                Blue_array[i-1 ,j-1] = (1/2)*(padded[i-1, j]+padded[i+1, j])
-            # At green pixel with blue adjacent
-            elif i%2 == 0 and j%2 != 0:
-                Red.append((1/2)*(padded[i-1, j]+padded[i+1, j]))
-#                np.append(Red_array, (1/2)*(padded[i-1, j]+padded[i+1, j]))
-                Red_array[i-1,j-1] = (1/2)*(padded[i-1, j]+padded[i+1, j])
-                Green.append(padded[i,j])
-                Green_array[i-1 ,j-1] = padded[i,j]
-                Blue.append((1/2)*(padded[i, j-1] + padded[i, j+1]))
-                Blue_array[i-1 ,j-1] = (1/2)*(padded[i, j-1] + padded[i, j+1])
-            # At blue pixel
-            elif i%2 == 0 and j%2 == 0:
-                Red.append((1/4)*(padded[i+1, j+1] + padded[i-1, j-1]+ padded[i-1, j+1] + padded[i+1, j-1]))
-#                np.append(Red_array, (1/4)*(padded[i+1, j+1] + padded[i-1, j-1]+ padded[i-1, j+1] + padded[i+1, j-1]))
-                Red_array[i-1,j-1] = (1/4)*(padded[i+1, j+1] + padded[i-1, j-1]+ padded[i-1, j+1] + padded[i+1, j-1])
-                Green.append((1/4)*(padded[i+1, j] + padded[i-1, j] + padded[i, j-1] + padded[i, j+1]))
-                Green_array[i-1 ,j-1] = (1/4)*(padded[i+1, j] + padded[i-1, j] + padded[i, j-1] + padded[i, j+1])
-                Blue.append(padded[i, j])
-                Blue_array[i-1 ,j-1] = padded[i, j]
-#print(Red)
-#print(np.array(Red))
-print()
-print("Red array:")
-print(Red_array)
-print("Blue array:")
-print(Blue_array)
-print("Green array:")
-print(Green_array)
-c = np.stack((Red_array, Green_array, Blue_array), axis = 2)
-print("Stacked array:")
-print(c)
-print(c.shape)
-#    for i in 
-#    print(normalized_img)
-#    print(normalized_img.size)
-#    print(normalized_img.shape)
-
+    demosaiced_img = demosaic(raw_image)
+    white_balanced_img = white_balance(demosaiced_img)
+    processed_img = curve_and_quantize(white_balanced_img, 0.85)
+    imageio.imwrite("Processed_Image.png", processed_img)
+    imageio.imwrite("Processed_Image.jpeg", processed_img)
--- HW1/raw.py
+++ HW1/raw.py
@@ -8,9 +8,37 @@ def demosaic(raw):
         The demosaiced image in 32-bit floating point [HxWx3]
   """
   normalized_img = raw.astype(np.float32) / (2**16 - 1)
-  
-
-  pass
+  padded = np.pad(normalized_img, pad_width=1, mode='constant', constant_values=0)
+  row = padded.shape[0] # num of rows of padded array
+  col = padded.shape[1] # num of col of padded array
+  Red_array= np.empty((normalized_img.shape[0], normalized_img.shape[1]))
+  Blue_array= np.empty((normalized_img.shape[0], normalized_img.shape[1]))
+  Green_array= np.empty((normalized_img.shape[0], normalized_img.shape[1]))
+  for i in range(1, row-1, 1): 
+      for j in range(1, col-1, 1):
+        # At red pixel
+        if i%2 != 0 and j%2 != 0:
+            Red_array[i-1 ,j-1] = padded[i, j]
+            Green_array[i-1 ,j-1] = np.mean([padded[i+1, j], padded[i-1, j], padded[i, j-1], padded[i, j+1]], dtype = np.float32)
+            Blue_array[i-1 ,j-1] = np.mean([padded[i+1, j+1], padded[i-1, j-1], padded[i-1, j+1], padded[i+1, j-1]], dtype = np.float32)
+        # At green pixel with red adjacent
+        elif i%2 != 0 and j%2 == 0:
+            Red_array[i-1,j-1] = np.mean([padded[i, j-1], padded[i, j+1]], dtype = np.float32)
+            Green_array[i-1 ,j-1] = padded[i,j]
+            Blue_array[i-1 ,j-1] = np.mean([padded[i-1, j], padded[i+1, j]], dtype = np.float32)
+        # At green pixel with blue adjacent
+        elif i%2 == 0 and j%2 != 0:
+            Red_array[i-1,j-1] = np.mean([padded[i-1, j], padded[i+1, j]], dtype = np.float32)
+            Green_array[i-1 ,j-1] = padded[i,j]
+            Blue_array[i-1 ,j-1] = np.mean([padded[i, j-1], padded[i, j+1]], dtype = np.float32)
+        # At blue pixel
+        elif i%2 == 0 and j%2 == 0:
+            Red_array[i-1,j-1] = np.mean([padded[i+1, j+1], padded[i-1, j-1], padded[i-1, j+1], padded[i+1, j-1]], dtype = np.float32)
+            Green_array[i-1 ,j-1] = np.mean([padded[i+1, j], padded[i-1, j], padded[i, j-1], padded[i, j+1]], dtype = np.float32)
+            Blue_array[i-1 ,j-1] = padded[i, j]
+  demosaic_img = np.stack((Red_array, Green_array, Blue_array), axis = 2)
+  demosaic_img = demosaic_img.astype(np.float32)
+  return demosaic_img
 
 def white_balance(image):
   """ White balanaces a 32-bit floating point demosaiced image.
@@ -20,7 +48,19 @@ def white_balance(image):
       Returns:
         The white balanced image in 32-bit floating point [HxWx3]
   """
-  pass
+  red_array = image[:, :, 0]
+  green_array = image[:, :, 1]
+  blue_array = image[:, :, 2]
+  red_scalar = 0.5/np.mean(red_array)
+  green_scalar = 0.5/np.mean(green_array)
+  blue_scalar = 0.5/np.mean(blue_array)
+  new_red = red_array*red_scalar
+  new_green = green_array*green_scalar
+  new_blue = blue_array*blue_scalar
+  white_balance_img = np.stack((new_red, new_green, new_blue), axis = 2)
+  white_balance_img = white_balance_img.astype(np.float32)
+  return white_balance_img
+
 
 def curve_and_quantize(image,inv_gamma=0.85):
   """ Applies inverse gamma function and quantizes to 8-bit.
@@ -30,5 +70,9 @@ def curve_and_quantize(image,inv_gamma=0.85):
       Returns:
         The curved and quantized image in 8-bit unsigned integer [HxWx3]
   """
-  pass
+  gamma_img = image**(inv_gamma)
+  clipped_img = np.clip(gamma_img, 0, 1)
+  out_img = clipped_img*255
+  out_img = out_img.astype(np.uint8)
+  return out_img
 
